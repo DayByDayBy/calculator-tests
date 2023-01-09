34a6bbff9dd05fb6e08f87f750cd2493f5a5e2a1
Diomedes Git <diomedesgit@gmail.com>
2023-01-09 15:43:11 +0000

    cy tests done, passing

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -8,12 +8,58 @@ describe("Calculator", () => {
     cy.get('.display').should('contain', '2')
   })
 
+  it('should update display when pressed', () => {
+    cy.get('#number3').click();
+    cy.get('.display').should('contain', '3')
+  })
 
-  
+  it('should chain multiple operations', () => {
+    cy.get('#number3').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '10')
+  })
 
+
+  it('should output negative', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-6')
+  })
+
+
+  it('should output decimals', () => {
+    cy.get('#number9').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '4.5')
+  })
+
+  it('should output large numbers', () => {
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '99989990001')
   })
 
 })
+
 
 
 
